ca77479b65db64c446506a4d35e50c0c0c8080f6
TECOAmber <pxprojectamber01@gmail.com>
2024-08-16 16:41:54 +0800

    first commit
    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+var ws
+
+// 監聽 click 事件
+document.querySelector('#connect')?.addEventListener('click', (e) => {
+    connect()
+})
+
+document.querySelector('#disconnect')?.addEventListener('click', (e) => {
+    disconnect()
+})
+
+document.querySelector('#sendBtn')?.addEventListener('click', (e) => {
+    const msg = document.querySelector('#sendMsg')
+    sendMessage(msg?.value)
+})
+
+function connect() { 
+    // Create WebSocket connection
+    //ws = new WebSocket('ws://localhost:8080') 
+    
+     ws = new WebSocket('ws://172.20.10.5:8080') 
+    // 在開啟連線時執行
+    ws.onopen = () => {
+        console.log('[open connection]')
+        // Listen for messages from Server
+        ws.onmessage = event => {
+            console.log(`[Message from server]:\n %c${event.data}` , 'color: blue')
+        }
+    }
+}
+
+function sendMessage(msg) {
+    // Send messages to Server
+    ws.send(msg)
+}
+
+function disconnect() {
+    ws.close()
+    // 在關閉連線時執行
+    ws.onclose = () => console.log('[close connection]')
+}
